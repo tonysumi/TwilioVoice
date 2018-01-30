d3070401f2c9a0e467cc0d51e6968b89bda3f89f
11CS116 <tony_achumi@yahoo.com>
2018-01-30 16:22:33 +0530

    header file

--- src/ios/TwilioVoicePlugin.h
+++ src/ios/TwilioVoicePlugin.h
@@ -1,7 +1,7 @@
 #import <Foundation/Foundation.h>
 #import <Cordova/CDV.h>
 
-@interface TwilioVoicePlugin : CDVPlugin <PKPushRegistryDelegate, TVOCallDelegate, TVONotificationDelegate, CXProviderDelegate> { }
+@interface TwilioVoicePlugin : CDVPlugin <TVOCallDelegate, TVONotificationDelegate> { }
 
 - (void) pluginInitialize;
 
@@ -17,40 +17,11 @@
 - (void) unmuteCall: (CDVInvokedUrlCommand*)command;
 - (void) isCallMuted: (CDVInvokedUrlCommand*)command;
 
-#pragma mark PKPushRegistryDelegate methods
-- (void)pushRegistry:(PKPushRegistry *)registry didUpdatePushCredentials:(PKPushCredentials *)credentials forType:(PKPushType)type;
-- (void)pushRegistry:(PKPushRegistry *)registry didInvalidatePushTokenForType:(PKPushType)type;
-- (void)pushRegistry:(PKPushRegistry *)registry didReceiveIncomingPushWithPayload:(PKPushPayload *)payload forType:(PKPushType)type;
-
-#pragma mark TVONotificationDelegate
-- (void)callInviteReceived:(TVOCallInvite *)callInvite;
-- (void)callinvitecanceled:(TVOCallInvite *)callInvite;
-- (void)notificationError:(NSError *)error;
-
-#pragma mark TVOCallDelegate
-- (void)callDidConnect:(TVOCall *)call
-- (void)callDidDisconnect:(TVOCall *)call;
-- (void)call:(TVOCall *)call didFailWithError:(NSError *)error;
-
-#pragma mark Conversion methods for the plugin
-- (NSString*) stringFromCallInviteState:(TVOCallInviteState)state;
-- (NSString*) stringFromCallState:(TVOCallState)state;
-
 #pragma mark Cordova Integration methods for the plugin Delegate 
 - (void) javascriptCallback:(NSString *)event withArguments:(NSDictionary *)arguments;
 - (void) javascriptCallback:(NSString *)event;
 - (void) javascriptErrorback:(NSError *)error;
 
-#pragma mark - CXProviderDelegate - based on Twilio Voice with CallKit Quickstart ObjC
-- (void)provider:(CXProvider *)provider performPlayDTMFCallAction:(CXPlayDTMFCallAction *)action;
-- (void)providerDidReset:(CXProvider *)provider;
-- (void)providerDidBegin:(CXProvider *)provider;
-- (void)provider:(CXProvider *)provider didActivateAudioSession:(AVAudioSession *)audioSession;
-- (void)provider:(CXProvider *)provider didDeactivateAudioSession:(AVAudioSession *)audioSession;
-- (void)provider:(CXProvider *)provider timedOutPerformingAction:(CXAction *)action;
-- (void)provider:(CXProvider *)provider performStartCallAction:(CXStartCallAction *)action;
-- (void)provider:(CXProvider *)provider performAnswerCallAction:(CXAnswerCallAction *)action;
-- (void)provider:(CXProvider *)provider performEndCallAction:(CXEndCallAction *)action;
 
 #pragma mark - CallKit Actions
 - (void)performStartCallActionWithUUID:(NSUUID *)uuid handle:(NSString *)handle;
